bfe1632b15765d3b87950466b7d1f8120eeebb9a
danka90 <domotor.daniel@gmail.com>
2015-06-07 09:01:09 +0200

    teszt

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,11 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'ZfDeals',
+        'DluTwBootstrap',
+        'ZendDeveloperTools',
+        'DoctrineModule',
+        'DoctrineORMModule',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
